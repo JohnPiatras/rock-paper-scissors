85ab0b45bfdcb4e2cf86861b78f742625740fcc3
burns <john.piatras@gmail.com>
2018-03-13 14:04:33 +0000

    Wrote computerPlay() function. Randomly returns rock, paper or scissors in a string.

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Rock, paper, scissors
+//John Burns
+
+function computerPlay() {
+    let choices = ["rock", "paper", "scissors"];
+    let n = Math.floor(Math.random() * 3);
+    return choices[n];
+}
+
+
+
+
+
